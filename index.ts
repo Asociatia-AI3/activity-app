4e6c0d9d4815c2bba6e34034d8454dd64e8f91f3
AndreeaStrut <strutandreeea@gmail.com>
2026-01-03 01:03:45 +0200

    Delete index.ts

--- index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20260102_214650 from './20260102_214650';
-
-export const migrations = [
-  {
-    up: migration_20260102_214650.up,
-    down: migration_20260102_214650.down,
-    name: '20260102_214650'
-  },
-];

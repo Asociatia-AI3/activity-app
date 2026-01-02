39a005a51c3ed475c0c809c1b875ee7924546891
AndreeaStrut <strutandreeea@gmail.com>
2026-01-03 01:05:27 +0200

    Add files via upload

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260102_214650 from './20260102_214650';
+
+export const migrations = [
+  {
+    up: migration_20260102_214650.up,
+    down: migration_20260102_214650.down,
+    name: '20260102_214650'
+  },
+];

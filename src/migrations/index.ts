5dbd947383305e9489f6bbba70a4dd6e7b1dac26
Saneok <sanduvasilcovan@gmail.com>
2026-01-06 21:09:21 +0200

    mesaj

---
+++ src/migrations/index.ts
@@ -0,0 +1,10 @@
+import * as migration_20260106_184841 from './20260106_184841';
+
+export const migrations = [
+  {
+    up: migration_20260106_184841.up,
+    down: migration_20260106_184841.down,
+    name: '20260106_184841'
+  },
+];
+~

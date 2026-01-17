078a7c3b11fa960870c6e56c8e1b23fcd73350b5
Adrian <Adrian.turcan.11@gmail.com>
2026-01-17 15:43:43 +0200

    First Commit

---
+++ src/collections/Schedule/index.ts
@@ -0,0 +1,12 @@
+import { CollectionConfig } from 'payload'
+
+export const Schedules: CollectionConfig = {
+  slug: 'schedules',
+  fields: [
+    { name: 'edition', type: 'relationship', relationTo: 'festival-editions', hasMany: false },
+    { name: 'startTime', type: 'date' },
+    { name: 'endTime', type: 'date' },
+    { name: 'activity', type: 'relationship', relationTo: 'activities', hasMany: false },
+    { name: 'location', type: 'relationship', relationTo: 'locations', hasMany: false },
+  ],
+}
---
+++ src/collections/Volunteers/index.ts
@@ -0,0 +1,16 @@
+import { CollectionConfig } from 'payload'
+
+export const Volunteers: CollectionConfig = {
+  slug: 'volunteers',
+  fields: [
+    { name: 'edition', type: 'relationship', relationTo: 'festival-editions', hasMany: false },
+    { name: 'name', type: 'text' },
+    { name: 'photo', type: 'upload', relationTo: 'media', hasMany: false },
+    { name: 'organization', type: 'text' },
+    { name: 'birthDate', type: 'date' },
+    { name: 'phone', type: 'text' },
+    { name: 'agreementDocument', type: 'upload', relationTo: 'media', hasMany: false },
+    { name: 'coordinator', type: 'relationship', relationTo: 'members', hasMany: false },
+    { name: 'userAccount', type: 'relationship', relationTo: 'users', hasMany: false },
+  ],
+}
